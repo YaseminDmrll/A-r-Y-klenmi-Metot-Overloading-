1f3a1f0edbbb0f428f46e3f2d792050e7660cec2
Yasemin DEMİREL <65770236+YaseminDmrll@users.noreply.github.com>
2020-05-24 14:21:13 +0300

    Update FinalUyg2.java

--- FinalUyg2/src/finaluyg2/FinalUyg2.java
+++ FinalUyg2/src/finaluyg2/FinalUyg2.java
@@ -1,8 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+Metotların Aşırı Yüklenmesi İf-Else ile Mantıksal Operatörlerle Çıkarma ve Bölme Uygulaması
+verilen iki sayının if-else  koşullarına göre  ekrana yazdıran
+dört metotdan oluşan  bir overloading uygulamasıdır.
+*/
 
 package finaluyg2;
 
